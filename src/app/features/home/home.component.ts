ed94742db32046eb77170060315284f288956264
Carlos Colmenares <ccolmenares@jacidi.com>
2024-01-10 17:13:55 -0400

    wip: swiper

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -4,13 +4,28 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 
+import { SwiperModule } from 'swiper/angular';
+
+import 'swiper/scss';
+import 'swiper/scss/navigation';
+import 'swiper/scss/pagination';
+
+// import Swiper core and required modules
+import SwiperCore, { Navigation, Pagination } from 'swiper';
+
+// install Swiper modules
+SwiperCore.use([Navigation, Pagination]);
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, SwiperModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
+  onSlideChange() {
+    console.log('slide change');
+  }
 }
