74164efeb43342f1562f64410a766a205619bfe9
carlos colmenares <cacg98@gmail.com>
2024-01-10 21:35:51 -0400

    wip: cards swiper

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -9,12 +9,13 @@ import { SwiperModule } from 'swiper/angular';
 import 'swiper/scss';
 import 'swiper/scss/navigation';
 import 'swiper/scss/pagination';
+import 'swiper/scss/effect-cards';
 
 // import Swiper core and required modules
-import SwiperCore, { Navigation, Pagination } from 'swiper';
+import SwiperCore, { Navigation, Pagination, EffectCards, SwiperOptions } from 'swiper';
 
 // install Swiper modules
-SwiperCore.use([Navigation, Pagination]);
+SwiperCore.use([Navigation, Pagination, EffectCards]);
 
 @Component({
   selector: 'app-home',
@@ -24,6 +25,12 @@ SwiperCore.use([Navigation, Pagination]);
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  config: SwiperOptions = {
+    navigation: true,
+    pagination: { clickable: true },
+    cardsEffect: { slideShadows: true }
+  };
 
   onSlideChange() {
     console.log('slide change');
