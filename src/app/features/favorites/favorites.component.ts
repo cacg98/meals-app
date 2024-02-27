d6b8c68331c7a6615b967c5801cd2b0a1c652dd3
Carlos Colmenares <ccolmenares@jacidi.com>
2024-02-27 17:02:57 -0400

    wip: display favorites

--- src/app/features/favorites/favorites.component.ts
+++ src/app/features/favorites/favorites.component.ts
@@ -2,12 +2,14 @@ import { Component, OnInit, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { IFavorite } from '../../common/interfaces/favorite';
+import { IPreviewRecipe } from '../../common/interfaces/meals-responses';
+import { RecipeCardComponent } from '../../common/components/recipe-card/recipe-card.component';
 import { LoaderService } from '../../common/services/loader/loader.service';
 
 @Component({
   selector: 'app-favorites',
   standalone: true,
-  imports: [],
+  imports: [RecipeCardComponent],
   templateUrl: './favorites.component.html',
   styleUrl: './favorites.component.scss',
 })
@@ -15,11 +17,22 @@ export default class FavoritesComponent implements OnInit {
   private route = inject(ActivatedRoute);
   private loaderService = inject(LoaderService);
 
-  favorites: IFavorite[] = [];
+  recipes: IPreviewRecipe[] = [];
 
   ngOnInit(): void {
+    // TODO revisar crear favorito sin difficulty o time
     this.route.data.subscribe(({ favorites }) => {
-      this.favorites = favorites;
+      this.recipes = (favorites as IFavorite[]).map((favorite) => {
+        const info = [];
+        if (favorite.difficulty) info.push(favorite.difficulty);
+        if (favorite.time) info.push(favorite.time);
+        return {
+          anchor: favorite.anchor,
+          image: favorite.image,
+          name: favorite.name,
+          info: info,
+        };
+      });
       Promise.resolve(null).then(() => this.loaderService.hideSpinner());
     });
   }
