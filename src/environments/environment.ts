625b0f5733d187db5b0148f269fcc8e012da1bf9
carlos colmenares <cacg98@gmail.com>
2024-03-18 17:16:30 -0400

    fix: prod env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://meals-api-9rkp.onrender.com',
+    apiUrl: 'https://meals-api-9rkp.onrender.com/api/v1',
     nestleUrl: 'https://www.recetasnestle.com.ve/'
 };
