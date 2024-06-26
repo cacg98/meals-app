d1acdf6165a625ef094a2a8535ad27c24a12bf9a
carlos colmenares <cacg98@gmail.com>
2024-06-25 20:26:34 -0400

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://meals-back.livelybay-8a7bca45.eastus.azurecontainerapps.io/api/v1',
+    apiUrl: 'https://meals.cacgdev.xyz/api/v1',
     nestleUrl: 'https://www.recetasnestle.com.ve/'
 };
